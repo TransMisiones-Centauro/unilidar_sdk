f78666862e22f0116456264c6da237005daa8f0f
Enric Cervera <ecervera@uji.es>
2024-05-08 12:14:03 +0200

    Fix tags

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t robinlab/unilidar_sdk:foxy .
+docker build -t robinlab/unilidar_sdk:humble .
--- run_rviz2.sh
+++ run_rviz2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rocker --nvidia --x11 robinlab/unilidar_sdk:foxy "rviz2 -d /ros2_ws/src/unitree_lidar_ros2/src/unitree_lidar_ros2/rviz/view.rviz"
+rocker --nvidia --x11 robinlab/unilidar_sdk:humble "rviz2 -d /ros2_ws/src/unitree_lidar_ros2/src/unitree_lidar_ros2/rviz/view.rviz"
